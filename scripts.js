6ad4698321157e31245de07428d48fe21f5ffaaf
Kothapalli Dinesh Reddy <dineshkothapalli77@gmail.com>
2025-01-14 07:10:29 +0530

    scripts.js

--- scripts.js
+++ scripts.js
@@ -13,9 +13,9 @@ function updateSkill(skillId, percentId, value) {
   }
   
   // Example usage to dynamically update percentages
-  updateSkill("html-css-bar", "html-css-percent", 95); // Update HTML & CSS
-  updateSkill("javascript-bar", "javascript-percent", 80); // Update JavaScript
-  updateSkill("Python-bar", "Python-percent", 40); // Update WordPress
-  updateSkill("MYSQL-bar", "MYSQL-percent", 55); // Update WordPress
-  updateSkill("Django-bar", "Django-percent", 55); // Update WordPress
+  updateSkill("html-css-bar", "html-css-percent", 90); // Update HTML & CSS
+  updateSkill("javascript-bar", "javascript-percent", 75); // Update JavaScript
+  updateSkill("Python-bar", "Python-percent", 85); // Update WordPress
+  updateSkill("MYSQL-bar", "MYSQL-percent", 80); // Update WordPress
+  updateSkill("Django-bar", "Django-percent", 70); // Update WordPress
   
